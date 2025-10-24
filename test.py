c592f2a8dc1209411832cefc5f0e63f4c4ae0a90
OrianeCrouzet <oriane.crouzet78@gmail.com>
2025-10-24 17:38:53 +0200

    modified class attribut to match the datasets

--- test.py
+++ test.py
@@ -11,19 +11,19 @@ import numpy as np
 
 
 
-G = nx.read_gml("datasets/Protein_GML/enzyme_1.gml")
+G = nx.read_gml("datasets/AIDS_GML/91.gml")
 # Assigner des labels aléatoires (par exemple 3 classes)
-labels = {}
-for node in G.nodes():
-    labels[node] = random.randint(0, 2)  # Classes 0, 1, 2
-nx.set_node_attributes(G, labels, 'label')
+#labels = {}
+#for node in G.nodes():
+#    labels[node] = random.randint(0, 2)  # Classes 0, 1, 2
+#nx.set_node_attributes(G, labels, 'label')
 
-print(f"\nGraphe avec labels : {G.number_of_nodes()} nœuds, {G.number_of_edges()} arêtes")
-print(f"Distribution des labels : {[list(labels.values()).count(i) for i in range(3)]}")
+#print(f"\nGraphe avec labels : {G.number_of_nodes()} nœuds, {G.number_of_edges()} arêtes")
+#print(f"Distribution des labels : {[list(labels.values()).count(i) for i in range(3)]}")
 
-def plot_graph(G):
+def plot_graph(G, class_attr):
     pos = nx.spring_layout(G)
-    node_labels = nx.get_node_attributes(G, 'label')
+    node_labels = nx.get_node_attributes(G, class_attr)
     unique_labels = set(node_labels.values())
     colors = plt.cm.rainbow(np.linspace(0, 1, len(unique_labels)))
     color_map = {label: colors[i] for i, label in enumerate(unique_labels)}
@@ -34,52 +34,52 @@ def plot_graph(G):
     plt.title("Graphe avec labels")
     plt.show()
 
-def edge_homophily(G):
+def edge_homophily(G, class_attr):
     total_of_edges = G.number_of_edges()
     homo_edges = 0
     for edge in G.edges():
-        if G.nodes[edge[0]]['label'] == G.nodes[edge[1]]['label']:
+        if G.nodes[edge[0]][class_attr] == G.nodes[edge[1]][class_attr]:
             homo_edges+=1
     edge_homophilic_measure = homo_edges / total_of_edges
     return edge_homophilic_measure
 
-def node_homophily(G):
+def node_homophily(G, class_attr):
     total_of_nodes = G.number_of_nodes()
     homo_node = 0
     for node in G.nodes():
         local_homo = 0
         for neighbor in G.neighbors(node):
-            if G.nodes[neighbor]['label'] == G.nodes[node]['label']:
+            if G.nodes[neighbor][class_attr] == G.nodes[node][class_attr]:
                 local_homo+=1
         local_homo = local_homo/G.degree(node)
         homo_node += local_homo
     homo_node = homo_node / total_of_nodes
     return homo_node
 
-def somme_degres_label_k(G, k):
+def somme_degres_label_k(G, k, class_attr):
     somme = 0
     for node in G.nodes():
-        if G.nodes[node]['label'] == k:
+        if G.nodes[node][class_attr] == k:
             somme += G.degree(node)
     return somme
 
 #on peut tomber sur 0 : c'est normal (je pense).
-def class_homophily(G):
-    m = set(nx.get_node_attributes(G, 'label').values())
+def class_homophily(G, class_attr):
+    m = set(nx.get_node_attributes(G, class_attr).values())
     homophilia = 0
     for label in m:
 
         somme_interieur = 0
         nk = 0
         for v in G.nodes():
-            if G.nodes[v]['label'] == label:
+            if G.nodes[v][class_attr] == label:
                 nk += 1
                 nb_homo_neighbor = 0
                 for neighbor in G.neighbors(v):
-                    if G.nodes[neighbor]['label'] == label:
+                    if G.nodes[neighbor][class_attr] == label:
                         nb_homo_neighbor += 1
                 somme_interieur += nb_homo_neighbor
-        Dk = somme_degres_label_k(G, label)
+        Dk = somme_degres_label_k(G, label, class_attr)
         somme_interieur = somme_interieur / Dk
         nksurn = nk / G.number_of_nodes()
         resultat_intermediaire = somme_interieur - nksurn
@@ -89,13 +89,13 @@ def class_homophily(G):
     homophilia = homophilia / (m.__len__())
     return homophilia
 
-def adjusted_homophily(G):
-    edje_homophily= edge_homophily(G)
-    m = set(nx.get_node_attributes(G, 'label').values())
+def adjusted_homophily(G, class_attr):
+    edje_homophily= edge_homophily(G, class_attr)
+    m = set(nx.get_node_attributes(G, class_attr).values())
     my_term = (2*G.number_of_edges())**2
     my_firstSum=0
     for label in m:
-        my_firstSum+=somme_degres_label_k(G,label)**2/my_term
+        my_firstSum+=somme_degres_label_k(G,label, class_attr)**2/my_term
 
     my_nominator= edje_homophily-my_firstSum
     my_deniminator = 1-my_firstSum
@@ -105,12 +105,15 @@ def adjusted_homophily(G):
 
 
 # Tests
-g_edge_homophily = edge_homophily(G)
-g_node_homophily = node_homophily(G)
-g_class_homophily = class_homophily(G)
-g_adjusted_homophily = adjusted_homophily(G)
+label_G = 'symbol'
+g_edge_homophily = edge_homophily(G, class_attr=label_G)
+g_node_homophily = node_homophily(G, class_attr=label_G)
+g_class_homophily = class_homophily(G, class_attr=label_G)
+g_adjusted_homophily = adjusted_homophily(G, class_attr=label_G)
+
 print(f"\nGraphe edge homophily : {g_edge_homophily}")
 print(f"Graphe node homophily : {g_node_homophily}")
 print(f"Graphe class homophily : {g_class_homophily}")
 print(f"Graphe adjusted homophily : {g_adjusted_homophily}")
-plot_graph(G)
+
+plot_graph(G, class_attr=label_G)
